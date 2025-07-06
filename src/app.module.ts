8fcc218a84ad2b576bc2cd9f94d924c9166a93cd
Kajal <kajalprasad824@gmail.com>
2025-07-07 00:00:52 +0530

    Entity Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { Users } from './users/users.entity';
+import { Reports } from './reports/reports.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
     database: 'db.sqlite',
-    entities: [],
+    entities: [Users, Reports],
     synchronize: true,
   }),
   UsersModule, ReportsModule],
