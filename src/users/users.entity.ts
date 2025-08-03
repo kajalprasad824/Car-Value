d77c610ed6d4af06954cb85802d2de620e368bdd
Kajal <kajalprasad824@gmail.com>
2025-08-03 21:12:14 +0530

    create vs save entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert,AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Users {
@@ -11,4 +11,19 @@ export class Users {
 
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User With id', this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate(){
+        console.log('Updated User with id',this.id);
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User with id', this.id);
+    }
 }
